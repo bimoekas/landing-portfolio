186df48e851bc30ec08f1d128116ff3f859b2707
Bimo Eka Saputra <107888020+bimoekas@users.noreply.github.com>
2025-04-10 23:14:44 +0700

    feat: typewriter

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,3 +1,5 @@
+import { Typewriter } from 'react-simple-typewriter'
+
 import { HomeContent } from '@/content/home'
 
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
@@ -18,7 +20,17 @@ const Home = () => {
       <div className="font-jetbrains mt-16 text-center">
         <h1 className="text-3xl">
           {HomeContent.title}{' '}
-          <span className="text-primary">{HomeContent.name}</span>
+          <span className="text-primary">
+            <Typewriter
+              words={[HomeContent.name]}
+              loop={false}
+              cursor
+              cursorStyle="_"
+              typeSpeed={100}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>
         </h1>
         <p className="mt-8 max-w-3xl">{HomeContent.description}</p>
       </div>
